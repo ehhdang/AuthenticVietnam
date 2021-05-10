7345374fe230869ed6feba538180852364647b78
Ha Dang <danghoangha1996@gmail.com>
2021-05-09 23:25:36 -0400

    modify environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import { domain, clientId, audience, serverUrl } from "../../auth_config.json";
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain,
+    clientId,
+    redirectUri: window.location.origin,
+    audience,
+  },
+  dev: {
+    serverUrl,
+  }
 };
