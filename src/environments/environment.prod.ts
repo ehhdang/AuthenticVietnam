540dbd91504d495bbea9e004b7fb0333dfafd111
Ha Dang <danghoangha1996@gmail.com>
2021-05-14 17:17:37 -0400

    modify environment.prod.ts for firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,12 @@
-import { domain, clientId, audience, serverUrl } from "../../auth_config.json";
-
 export const environment = {
   production: true,
   auth: {
-    domain,
-    clientId,
+    domain: "dev-gmqoz3lk.us.auth0.com",
+    clientId: "Q4iSoeimXNuRA9mQAG6DKxPDaoS5w15D",
     redirectUri: window.location.origin,
-    audience,
+    audience: "https://express.jasmine.com",
   },
   dev: {
-    serverUrl,
+    serverUrl: "https://authenticvietnam-e6ee0.uc.r.appspot.com/",
   }
 };
