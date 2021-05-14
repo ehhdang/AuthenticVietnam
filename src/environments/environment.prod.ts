827c6f26dac00088677f6885abb60df6724f327e
Emma Dang <54388429+mysweetcalico@users.noreply.github.com>
2021-05-14 19:44:17 -0400

    Production (#1)
    * modify environment.prod.ts for firebase hosting
    
    * update env vars; add app icon and logo; fix alignment in login and register dialog

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,8 @@
-import { domain, clientId, audience, serverUrl } from "../../auth_config.json";
+import { prodServerUrl as serverUrl } from "../../auth_config.json";
 
 export const environment = {
   production: true,
-  auth: {
-    domain,
-    clientId,
-    redirectUri: window.location.origin,
-    audience,
-  },
   dev: {
-    serverUrl,
+    serverUrl
   }
 };
