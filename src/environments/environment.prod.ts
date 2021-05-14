43ce4fbfb9e7cb122dde0356646344d0cdc80583
Ha Dang <danghoangha1996@gmail.com>
2021-05-14 19:42:23 -0400

    update env vars; add app icon and logo; fix alignment in login and register dialog

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,8 @@
+import { prodServerUrl as serverUrl } from "../../auth_config.json";
+
 export const environment = {
   production: true,
-  auth: {
-    domain: "dev-gmqoz3lk.us.auth0.com",
-    clientId: "Q4iSoeimXNuRA9mQAG6DKxPDaoS5w15D",
-    redirectUri: window.location.origin,
-    audience: "https://express.jasmine.com",
-  },
   dev: {
-    serverUrl: "https://authenticvietnam-e6ee0.uc.r.appspot.com/",
+    serverUrl
   }
 };
